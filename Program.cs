ec6e49e9a10b235c59a08494f90fd95e767f7778
Leonardo Bitencourt <leonardobitencourt2020@gmail.com>
2023-01-04 02:59:00 -0300

    usando a classe Thread para fazer o console dormir por 1s

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StopwatchDotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (time != currentTime)
+            {
+                currentTime++;
+                Console.Clear();
+                Console.WriteLine(currentTime);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
